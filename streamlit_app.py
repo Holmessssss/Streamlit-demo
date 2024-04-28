bd30f90bc953cc7ee7a19a36ddf440d7abea254a
hetenbin <hetenbin123@gmail.com>
2024-04-28 20:34:57 +0800

    demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
-st.title('我的第一个Streamlit应用')
-st.write('欢迎来到Streamlit的世界！')
+st.title('欢迎!')
+name = st.text_input("请输入你的姓名: ")
 
-sidebar_input = st.sidebar.text_input("在这里输入内容")
-sidebar_slider = st.sidebar.slider("选择一个数值", 0, 100)
+if name is None:
+    st.write(f'我叫何藤斌.')
+    st.write(f'Hi, My name is Tenbin He, Nice to meet you.')
+else:
+    st.write(f'我叫{name}.')
+    st.write(f'Hi, My name is {name}, Nice to meet you.')
+
+
 
