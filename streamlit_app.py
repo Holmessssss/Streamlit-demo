dee0d1432a0c1447e51a0e3c510a0d76c0853e4a
hetenbin <hetenbin123@gmail.com>
2024-04-28 20:20:16 +0800

    demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title("Streamlit")
-st.write("This is Streamlit")
-st.sidebar.slider(0, 100)
+st.title('我的第一个Streamlit应用')
+st.write('欢迎来到Streamlit的世界！')
+
+sidebar_input = st.sidebar.text_input("在这里输入内容")
+sidebar_slider = st.sidebar.slider("选择一个数值", 0, 100)
+
