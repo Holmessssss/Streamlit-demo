b35fd61099382a869484d16dafc029fee2e9c375
hetenbin <hetenbin123@gmail.com>
2024-04-28 20:37:03 +0800

    demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('欢迎!')
 name = st.text_input("请输入你的姓名: ")
 
-if name is None:
+if name == " ":
     st.write(f'我叫何藤斌.')
     st.write(f'Hi, My name is Tenbin He, Nice to meet you.')
 else:
